2b2c23b8ead59d48681564d81fe196f8f525f09b
engineerJPark <jshackist@gmail.com>
2023-01-02 13:26:50 +0900

    edited train, val, util

---
+++ step/val.py
@@ -0,0 +1,36 @@
+
+import torch
+from torch.backends import cudnn
+cudnn.enabled = True
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+import importlib
+
+import voc12.dataloader
+from misc import pyutils, torchutils
+
+
+def validate(model, data_loader):
+    print('validating ... ', flush=True, end='')
+
+    val_loss_meter = pyutils.AverageMeter('loss1', 'loss2')
+
+    model.eval()
+
+    with torch.no_grad():
+        for pack in data_loader:
+            img = pack['img']
+
+            label = pack['label'].cuda(non_blocking=True)
+
+            x = model(img)
+            loss1 = F.multilabel_soft_margin_loss(x, label)
+
+            val_loss_meter.add({'loss1': loss1.item()})
+
+    model.train()
+
+    print('loss: %.4f' % (val_loss_meter.pop('loss1')))
+
+    return
