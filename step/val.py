288b05ff594fba1ddcb07283aee7fadacb84a18c
engineerJPark <jshackist@gmail.com>
2023-01-03 10:56:09 +0900

    add validation  codes

--- step/val.py
+++ step/val.py
@@ -1,36 +1,42 @@
-
 import torch
-from torch.backends import cudnn
-cudnn.enabled = True
-from torch.utils.data import DataLoader
+import torch.nn as nn
 import torch.nn.functional as F
-
+from torch.utils.data import DataLoader
+from val import validate
 import importlib
 
-import voc12.dataloader
-from misc import pyutils, torchutils
-
+from voc12 import dataloader
+from utils import pyutils, torchutils, imutils
 
 def validate(model, data_loader):
-    print('validating ... ', flush=True, end='')
-
-    val_loss_meter = pyutils.AverageMeter('loss1', 'loss2')
-
     model.eval()
+    print('validating ... ', flush=True, end='')
+    val_loss_meter = pyutils.AverageMeter('loss1', 'loss2')
 
     with torch.no_grad():
         for pack in data_loader:
-            img = pack['img']
-
+            # img, label & cuda
+            img = pack['img'].cuda(non_blocking=True)
+            sal_img = pack['sal_img'].cuda(non_blocking=True)
             label = pack['label'].cuda(non_blocking=True)
+            
+            # prediction
+            x, x_cam = model(img)
+            
+            # classification loss
+            loss_cls = F.multilabel_soft_margin_loss(x, label) # for predicted label and GT lable
+            
+            # saliency loss ... need to be fixed : sal_img should come from dataloader
+            fg, bg = imutils.cam2fg_n_bg(x_cam, sal_img, label) # label should be one hot decoded
+            pred_sal = imutils.psuedo_saliency(fg, bg)
+            loss_sal = F.mse_loss(pred_sal, sal_img) # for pseudo sal map & saliency map
+            
+            # total loss
+            loss_total = loss_cls + loss_sal
 
-            x = model(img)
-            loss1 = F.multilabel_soft_margin_loss(x, label)
+            # adding total loss
+            val_loss_meter.add({'loss': loss_total.item()})
 
-            val_loss_meter.add({'loss1': loss1.item()})
-
+    print('loss: %.4f' % (val_loss_meter.pop('loss')))
     model.train()
-
-    print('loss: %.4f' % (val_loss_meter.pop('loss1')))
-
     return
