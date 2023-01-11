3d5c599e9b8a0036ac74e9cb1fca01fa23ee6f28
engineerJPark <jshackist@gmail.com>
2023-01-11 13:14:50 +0900

    edited main.py

--- main.py
+++ main.py
@@ -37,10 +37,14 @@ if __name__ == '__main__':
                         help="Path to VOC 2012 Devkit, must contain ./JPEGImages as subdirectory.")
     parser.add_argument("--sal_root", default='dataset/SALImages', type=str,
                         help="Path to VOC 2012 Devkit, must contain ./SALImage as subdirectory.")
+    parser.add_argument("--network", default="net.resnet38_base", type=str)
+    parser.add_argument("--n_gpus", type=int, default=torch.cuda.device_count())
+    parser.add_argument("--n_processes_per_gpu", nargs='*', type=int)
+    parser.add_argument("--n_total_processes", default=1, type=int)
 
     # Dataset
     parser.add_argument("--train_list", default="voc12/train.txt", type=str)
-    parser.add_argument("--val_list", default="voc12/val.txt", type=str)
+    parser.add_argument("--val_list", default="voc12/train.txt", type=str)
     parser.add_argument("--infer_list", default="voc12/train.txt", type=str,
                         help="voc12/train_aug.txt to train a fully supervised model, "
                              "voc12/train.txt or voc12/val.txt to quickly check the quality of the labels.")
@@ -56,37 +60,19 @@ if __name__ == '__main__':
     parser.add_argument("--tau", default=0.5, type=float)
     parser.add_argument("--lam", default=0.5, type=float)
 
-    # Class Activation Map
-    parser.add_argument("--cam_network", default="net.resnet38", type=str)
-    parser.add_argument("--cam_crop_size", default=512, type=int)
-    parser.add_argument("--cam_batch_size", default=8, type=int) # was 16
-    # parser.add_argument("--cam_num_epoches", default=5, type=int)
-    parser.add_argument("--cam_learning_rate", default=0.1, type=float)
-    parser.add_argument("--cam_weight_decay", default=1e-4, type=float)
-    parser.add_argument("--cam_eval_thres", default=0.15, type=float)
+    # Class Activation Map parameter
     parser.add_argument("--cam_scales", default=(1.0, 0.5, 1.5, 2.0),
                         help="Multi-scale inferences")
+    parser.add_argument("--cam_npy", default="savefile/result/cam", type=str) # "savefile/result/cam" , separate two of them by .npy & .png
+    parser.add_argument("--cam_png", default=None, type=str) # "savefile/result/cam" , 
+    parser.add_argument("--thr", default=0.20, type=float)
 
-    parser.add_argument("--network", default="net.resnet38_base", type=str)
-    # parser.add_argument("--weights", required=True, type=str)
-    parser.add_argument("--n_gpus", type=int, default=torch.cuda.device_count())
-    # parser.add_argument("--infer_list", default="voc12/train.txt", type=str)
-    parser.add_argument("--n_processes_per_gpu", nargs='*', type=int)
-    parser.add_argument("--n_total_processes", default=1, type=int)
-    # parser.add_argument("--img_root", default='VOC2012', type=str)
-    
     ### CRF parameter
     parser.add_argument("--crf", default=None, type=str)
     parser.add_argument("--crf_alpha", nargs='*', type=int)
     parser.add_argument("--crf_t", nargs='*', type=int)
     
-    ### CAM parameter
-    parser.add_argument("--cam_npy", default="savefile/result/cam", type=str) # "savefile/result/cam" , separate two of them by .npy & .png
-    parser.add_argument("--cam_png", default=None, type=str) # "savefile/result/cam" , 
-    parser.add_argument("--thr", default=0.20, type=float)
-    
-    # Evaluation
-    # need to be fixed
+    # Evaluation, need to be fixed
     parser.add_argument('--dataset', default='voc12')
     parser.add_argument('--datalist', default='voc12/train.txt', type=str)
     parser.add_argument('--gt_dir', default='dataset/VOCdevkit/VOC2012/SegmentationClass', type=str)
